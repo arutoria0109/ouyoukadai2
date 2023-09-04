768f084edf2b73283c86765f76cb6a4ebd2949ea
arutoria0109 <samanimaya017@gmail.com>
2023-09-04 18:41:48 +0000

    [add]応用課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,13 @@ class BooksController < ApplicationController
     @books = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
     @books = Book.all
     @book = Book.new
+    
   end
 
   def create
---
+++ app/models/favorite.rb
@@ -0,0 +1,6 @@
+class Favorite < ApplicationRecord
+
+  belongs_to :uesr
+  belongs_to :book
+
+end
