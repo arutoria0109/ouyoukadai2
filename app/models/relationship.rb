e7c4bf84755ed856372730e40f7dfce3de8b4f5e
arutoria0109 <samanimaya017@gmail.com>
2023-09-05 18:14:56 +0000

    [finish]応用課題4

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,22 +3,6 @@ class Relationship < ApplicationRecord
      belongs_to :follower, class_name: "User"
      belongs_to :followed, class_name: "User"
 
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     #followerテーブルとfollowedテーブルを探しに行ってしまうため
     #class_name: "User"でuserテーブルからデータを回収する
